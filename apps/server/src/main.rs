b80eeaa8d5fed310e077d76aa060136c8bf20709
paring <paringchan@proton.me>
2026-06-04 05:52:45 +0900

    refactor(server): extract config structs into config.rs

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,10 +1,12 @@
+mod config;
+
 use clap::Parser;
+use config::AppConfig;
 use figment::{
     Figment,
     providers::{Env, Format, Toml},
 };
 use poem::{Route, Server, handler, listener::TcpListener};
-use serde::Deserialize;
 
 #[macro_use]
 extern crate tracing;
@@ -25,35 +27,6 @@ pub struct Args {
         default_value = "kanade-server.toml"
     )]
     pub config: String,
-}
-
-#[derive(Debug, Deserialize, Default)]
-pub struct AppConfig {
-    #[serde(default)]
-    pub server: ServerConfig,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct ServerConfig {
-    pub host: String,
-    pub port: u16,
-}
-
-impl Default for ServerConfig {
-    fn default() -> Self {
-        Self {
-            host: default_host(),
-            port: default_port(),
-        }
-    }
-}
-
-fn default_host() -> String {
-    "0.0.0.0".to_string()
-}
-
-fn default_port() -> u16 {
-    4000
 }
 
 #[tokio::main]
