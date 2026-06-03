960a0f0841df07aef147d68afeeb40702d13ecc4
paring <paringchan@proton.me>
2026-06-04 04:53:15 +0900

    setup tracing

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate tracing;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .init();
+
+    info!("Kanade Server v{} starting...", env!("CARGO_PKG_VERSION"));
 }
+
