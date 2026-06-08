a61734f726ec24fbaa59f9effa68a0de16dc2dac
paring <paringchan@proton.me>
2026-06-09 01:29:18 +0900

    feat(agent): set up tracing and tokio

--- apps/agent/src/main.rs
+++ apps/agent/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, fmt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
     println!("Hello, world!");
 }
