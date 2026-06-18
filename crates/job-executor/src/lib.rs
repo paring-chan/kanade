e708f4099e446315916be6830c981b912d57c7c3
paring <paringchan@proton.me>
2026-06-19 03:12:32 +0900

    저는저능아입니다

--- crates/job-executor/src/lib.rs
+++ crates/job-executor/src/lib.rs
@@ -88,7 +88,7 @@ impl<R: adapter::JobStatusReport> JobExecutor<R> {
         {
             use std::os::unix::fs::PermissionsExt;
             tokio::fs::set_permissions(dir.path(), std::fs::Permissions::from_mode(0o644)).await?;
-            tokio::fs::set_permissions(&ssh_dir, std::fs::Permissions::from_mode(0o700)).await?;
+            tokio::fs::set_permissions(&ssh_dir, std::fs::Permissions::from_mode(0o600)).await?;
         }
 
         let mut key_file = tokio::fs::File::create(ssh_dir.join("id_ed25519")).await?;
